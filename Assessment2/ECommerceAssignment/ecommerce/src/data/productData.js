5fa8d799274f92d0d62bea23462cbf41cf141a53
soumyalakshmi06 <78637945+soumyalakshmi06@users.noreply.github.com>
2023-02-23 09:24:49 +0530

    Add files via upload

---
+++ Assessment2/ECommerceAssignment/ecommerce/src/data/productData.js
@@ -0,0 +1,38 @@
+const productData = [
+    {
+      "title": "Brown eggs",
+      "type": "dairy",
+      "description": "Raw organic brown eggs in a basket",
+      "url": "https://via.placeholder.com/150x150.png?text=Product1",
+      "price": 28.1,
+      "rating": 4
+    },
+    {
+      "title": "Sweet fresh strawberry",
+      "type": "fruit",
+      "description": "Sweet fresh strawberry on the wooden table",
+      "url": "https://via.placeholder.com/150x150.png?text=Product2",
+      "price": 29.45,
+      "rating": 4
+    },
+    {
+      "title": "choclate",
+      "type": "fruit",
+      "description": "Sweet fresh strawberry on the wooden table",
+      "url": "https://via.placeholder.com/150x150.png?text=Product2",
+      "price": 29.45,
+      "rating": 4
+    },
+    {
+      "title": "apple",
+      "type": "fruit",
+      "description": "Sweet fresh strawberry on the wooden table",
+      "url": "https://via.placeholder.com/150x150.png?text=Product2",
+      "price": 29.45,
+      "rating": 4
+    }
+
+  ];
+  
+  export default productData;
+  
