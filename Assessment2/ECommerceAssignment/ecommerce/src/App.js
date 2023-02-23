8cc4e713991ee0f819125a080c3790f7ff2a6fc7
soumyalakshmi06 <78637945+soumyalakshmi06@users.noreply.github.com>
2023-02-23 09:33:28 +0530

    Add files via upload

---
+++ Assessment2/ECommerceAssignment/ecommerce/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/header";
+import ProductList from "./components/ProductList";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ProductList />
+    </div>
+  );
+}
+export default App;
