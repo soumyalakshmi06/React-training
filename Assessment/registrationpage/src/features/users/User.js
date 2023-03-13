0ccf5fc20af6aafcf3552cec2809eaa70ce3e1cb
soumyalakshmi06 <78637945+soumyalakshmi06@users.noreply.github.com>
2023-03-13 18:27:39 +0530

    Add files via upload

---
+++ Assessment/registrationpage/src/features/users/User.js
@@ -0,0 +1,70 @@
+const users = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets"
+      }
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv",
+      phone: "010-692-6593 x09125",
+      website: "anastasia.net",
+      company: {
+        name: "Deckow-Crist",
+        catchPhrase: "Proactive didactic contingency",
+        bs: "synergize scalable supply-chains"
+      }
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+      username: "Samantha",
+      email: "Nathan@yesenia.net",
+      phone: "1-463-123-4447",
+      website: "ramiro.info",
+      company: {
+        name: "Romaguera-Jacobson",
+        catchPhrase: "Face to face bifurcated interface",
+        bs: "e-enable strategic applications"
+      }
+    },
+    {
+      id: 4,
+      name: "Patricia Lebsack",
+      username: "Karianne",
+      email: "Julianne.OConner@kory.org",
+      phone: "493-170-9623 x156",
+      website: "kale.biz",
+      company: {
+        name: "Robel-Corkery",
+        catchPhrase: "Multi-tiered zero tolerance productivity",
+        bs: "transition cutting-edge web services"
+      }
+    },
+    {
+      id: 5,
+      name: "Chelsey Dietrich",
+      username: "Kamren",
+      email: "Lucio_Hettinger@annie.ca",
+      phone: "(254)954-1289",
+      website: "demarco.info",
+      company: {
+        name: "Keebler LLC",
+        catchPhrase: "User-centric fault-tolerant solution",
+        bs: "revolutionize end-to-end systems"
+      }
+    }
+  ];
+  
+  export default users;
+  
---
+++ Assessment/registrationpage/src/features/users/UserList.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@material-ui/core';
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setUsers(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Website</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+              <TableCell>{user.website}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default UserList;
